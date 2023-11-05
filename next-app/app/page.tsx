5354c23dfa0d7f87ab3e28bb54317c496315ddfe
NakuRei <nakurei7901@gmail.com>
2023-11-05 21:21:34 +0900

    fix: prevent main content cut-off when vertically long
    This commit ensures that the main content is no longer cut off at the
    top when it extends beyond the vertical length of the page.
    
    Closes #3

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,51 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
+      main1 <br />
+      main2 <br />
+      main3 <br />
       main <br />
       main <br />
       main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main <br />
+      main3 <br />
+      main2 <br />
+      main1 <br />
     </div>
   );
 }
