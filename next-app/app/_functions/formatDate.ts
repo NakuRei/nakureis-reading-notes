17a3d97bc1592328d8e960576faaa36ab7440c64
NakuRei <118409499+NakuRei@users.noreply.github.com>
2023-11-06 23:19:18 +0900

    feat: reflect config yaml info to main page (#15)
    * build: add js-yaml dependencies
    
    * feat: reflect config yaml info to main page
    
    Closes #8

---
+++ next-app/app/_functions/formatDate.ts
@@ -0,0 +1,7 @@
+export default function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(date);
+}
