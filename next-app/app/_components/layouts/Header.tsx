9b924d67d4ae68afac76ff771014789324411556
NakuRei <118409499+NakuRei@users.noreply.github.com>
2023-11-12 15:28:20 +0900

    feat: implement books page (#32)
    * feat: add new book configs
    
    * feat: implement books page
    
    * feat: add swap button for swap page
    
    Closes #6

--- next-app/app/_components/layouts/Header.tsx
+++ next-app/app/_components/layouts/Header.tsx
@@ -1,5 +1,7 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
+
+import { SquaresFour, SquareSplitVertical } from '@phosphor-icons/react';
 
 import ToggleModeCheckbox from './ToggleModeCheckbox';
 
@@ -16,6 +18,16 @@ export default function Header(props: HeaderProps) {
     router.push(props.homePath);
   }
 
+  const pathname = usePathname();
+  const isHome = pathname === props.homePath;
+  function changeShow() {
+    if (isHome) {
+      router.push('/books');
+      return;
+    }
+    router.push(props.homePath);
+  }
+
   return (
     <header className={`${props.className}`}>
       <div className="navbar">
@@ -28,6 +40,11 @@ export default function Header(props: HeaderProps) {
           </button>
         </div>
         <div className="navbar-end mr-3">
+          <label className="swap swap-rotate mr-3">
+            <input type="checkbox" onChange={changeShow} checked={isHome} />
+            <SquaresFour className="swap-off" size={32} />
+            <SquareSplitVertical className="swap-on" size={32} />
+          </label>
           <ToggleModeCheckbox />
         </div>
       </div>
---
+++ next-app/app/books/page.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import getBooks from '@/app/_functions/getBooks';
+
+export default async function BooksPage() {
+  const books = await getBooks();
+
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-[5%] md:gap-[5%] lg:gap-10 mx-7 sm:mx-20 lg:mx-20 my-[5%] md:my-7 lg:my-10 max-w-[1024px]">
+        {books.map((book) => (
+          <Link key={book.isbn} href={`/books/${book.isbn}`}>
+            <div className="flex flex-col justify-center items-center rounded-lg shadow-md shadow-neutral-600 overflow-hidden">
+              <figure className="w-full mx-auto">
+                <Image
+                  src={book.image}
+                  alt={`Cover of ${book.title}`}
+                  width={150}
+                  height={210}
+                  className="w-full h-auto object-contain"
+                  priority={true}
+                />
+              </figure>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
