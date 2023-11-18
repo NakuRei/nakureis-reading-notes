9f18265c41ca93e4d6859c729483eae3e244ffd4
NakuRei <nakurei7901@gmail.com>
2023-11-19 01:43:08 +0900

    feat(page): update home and books page style

--- next-app/app/books/page.tsx
+++ next-app/app/books/page.tsx
@@ -2,75 +2,30 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import getBooks from '@/app/_functions/getBooks';
-import formatDate from '@/app/_functions/formatDate';
 
 export default async function BooksPage() {
   const books = await getBooks();
-  const rakutenBooksUrl =
-    process.env.RAKUTEN_AFFILIATE_RAKUTEN_BOOKS_URL ?? '/';
 
   return (
-    <div>
-      {books.map((book) => (
-        <div key={book.isbn}>
-          <div className="flex card card-bordered md:card-side bg-base-200 shadow-xl mx-[5%] lg:mx-[15%] my-10 py-5 pr-0 pl-0 md:pl-8">
-            <figure className="flex-shrink-0">
-              <Image
-                src={book.image}
-                alt={`Cover of ${book.title}`}
-                width={150}
-                height={210}
-                className="w-[150px] h-[210px]"
-                priority={true}
-              />
-            </figure>
-            <div className="card-body flex-grow flex-shrink min-w-0">
-              <h2 className="card-title">{book.title}</h2>
-              <p className="text-sm">{book.publisherName}</p>
-              <div className="flex flex-row gap-2">
-                {book.categories?.map((category) => (
-                  <div key={category} className="badge badge-secondary">
-                    {category}
-                  </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 text-sm">
-                <div>
-                  <p>
-                    {book.finishDate
-                      ? `${formatDate(book.finishDate)} 読了`
-                      : '読了日不明'}
-                  </p>
-                </div>
-                <div>
-                  <p>
-                    {book.pageCount
-                      ? `${book.pageCount} ページ`
-                      : 'ページ数不明'}
-                  </p>
-                </div>
-                <div>
-                  {book.itemPrice ? `${book.itemPrice} 円` : '価格不明'}
-                </div>
-              </div>
-              <div className="card-actions justify-end">
-                <Link href={`/books/${book.isbn}`} className="btn btn-primary">
-                  {book.chapters ? book.chapters.length : 0} Notes
-                </Link>
-                <a
-                  href={book.affiliateUrl ?? rakutenBooksUrl}
-                  className="btn btn-accent"
-                  target="_blank"
-                  rel="noopener"
-                  aria-label="Buy this book"
-                >
-                  Buy
-                </a>
-              </div>
+    <div className="flex justify-center">
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-[5%] md:gap-[5%] lg:gap-10 mx-7 sm:mx-20 lg:mx-20 my-[5%] md:my-7 lg:my-10 max-w-[1024px]">
+        {books.map((book) => (
+          <Link key={book.isbn} href={`/books/${book.isbn}`}>
+            <div className="flex flex-col justify-center items-center rounded-lg shadow-md shadow-neutral-600 overflow-hidden">
+              <figure>
+                <Image
+                  src={book.image}
+                  alt={`Cover of ${book.title}`}
+                  width={150}
+                  height={210}
+                  priority={true}
+                  className="h-auto w-auto object-contain"
+                ></Image>
+              </figure>
             </div>
-          </div>
-        </div>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -2,28 +2,69 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import getBooks from '@/app/_functions/getBooks';
+import formatDate from '@/app/_functions/formatDate';
 
 export default async function Home() {
   const books = await getBooks();
+  const rakutenBooksUrl =
+    process.env.RAKUTEN_AFFILIATE_RAKUTEN_BOOKS_URL ?? '/';
 
   return (
     <div className="flex justify-center">
-      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-[5%] md:gap-[5%] lg:gap-10 mx-7 sm:mx-20 lg:mx-20 my-[5%] md:my-7 lg:my-10 max-w-[1024px]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-[90%] px-10">
         {books.map((book) => (
-          <Link key={book.isbn} href={`/books/${book.isbn}`}>
-            <div className="flex flex-col justify-center items-center rounded-lg shadow-md shadow-neutral-600 overflow-hidden">
-              <figure className="w-full mx-auto">
+          <div
+            key={book.isbn}
+            className="card bg-base-200 shadow-md rounded-lg border-[1px] border-neutral-400 border-opacity-60 m-3 flex flex-col justify-between px-2 pb-2 gap-2"
+          >
+            <div className="flex flex-col gap-2">
+              <figure>
                 <Image
                   src={book.image}
                   alt={`Cover of ${book.title}`}
                   width={150}
                   height={210}
-                  className="w-full h-auto object-contain"
                   priority={true}
-                />
+                  className="h-[105px] sm:h-[140px] w-auto"
+                ></Image>
               </figure>
+              <h2 className="card-title text-sm">{book.title}</h2>
             </div>
-          </Link>
+
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-row gap-2">
+                {book.categories?.map((category) => (
+                  <div key={category} className="badge badge-secondary">
+                    {category}
+                  </div>
+                ))}
+              </div>
+              <div>
+                <p className="text-sm">
+                  {book.finishDate
+                    ? `${formatDate(book.finishDate)} 読了`
+                    : '読了日不明'}
+                </p>
+              </div>
+              <div className="flex flex-row justify-end gap-2">
+                <Link
+                  href={`/books/${book.isbn}`}
+                  className="btn btn-sm btn-primary"
+                >
+                  {book.chapters ? book.chapters.length : 0} Notes
+                </Link>
+                <a
+                  href={book.affiliateUrl ?? rakutenBooksUrl}
+                  className="btn btn-sm btn-accent"
+                  target="_blank"
+                  rel="noopener"
+                  aria-label="Buy this book"
+                >
+                  Buy
+                </a>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
