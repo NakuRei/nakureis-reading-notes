47308a21c5dbbf074d2515b61049011dacc3b85c
NakuRei <nakurei7901@gmail.com>
2023-11-19 02:23:54 +0900

    feat: add logging for Rakuten API failure responses
    - Record status code in logs when Rakuten API requests fail.
    - Include status text in logs for failed API requests.

--- next-app/app/_functions/getBooksApiRakuten.ts
+++ next-app/app/_functions/getBooksApiRakuten.ts
@@ -37,32 +37,41 @@ export default async function getBooksApiRakuten(
   const format = 'json';
   const affiliateId = process.env.RAKUTEN_AFFILIATE_ID;
   const applicationId = process.env.RAKUTEN_APPLICATION_ID;
-  const response = await fetch(
-    `${endpoint}/20170404?format=${format}&isbnjan=${isbnJan}&affiliateId=${affiliateId}&applicationId=${applicationId}`,
-  );
-  if (!response.ok) {
-    throw new Error('Failed to fetch books from Rakuten');
+
+  try {
+    const response = await fetch(
+      `${endpoint}/20170404?format=${format}&isbnjan=${isbnJan}&affiliateId=${affiliateId}&applicationId=${applicationId}`,
+    );
+    if (!response.ok) {
+      console.error(
+        `Error fetching book data for ISBN ${isbnJan}: ${response.status} ${response.statusText}`,
+      );
+      throw new Error('Failed to fetch books from Rakuten');
+    }
+    const jsonData: RakutenBooksApiResponse = await response.json();
+
+    return jsonData.Items.map((elem: RakutenBookJson) => {
+      const imageUrl =
+        elem.Item.largeImageUrl ||
+        elem.Item.mediumImageUrl ||
+        elem.Item.smallImageUrl ||
+        '';
+      const imageUrlHttps = imageUrl.replace(/^http:/, 'https:');
+
+      return {
+        isbn: elem.Item.isbn,
+        title: elem.Item.title,
+        author: elem.Item.author,
+        publisherName: elem.Item.publisherName,
+        salesDate: elem.Item.salesDate,
+        itemCaption: elem.Item.itemCaption,
+        itemPrice: elem.Item.itemPrice,
+        imageUrl: imageUrlHttps,
+        affiliateUrl: elem.Item.affiliateUrl,
+      };
+    });
+  } catch (error) {
+    console.error(`Error in getBooksApiRakuten for ISBN ${isbnJan}:`, error);
+    throw error;
   }
-  const jsonData: RakutenBooksApiResponse = await response.json();
-
-  return jsonData.Items.map((elem: RakutenBookJson) => {
-    const imageUrl =
-      elem.Item.largeImageUrl ||
-      elem.Item.mediumImageUrl ||
-      elem.Item.smallImageUrl ||
-      '';
-    const imageUrlHttps = imageUrl.replace(/^http:/, 'https:');
-
-    return {
-      isbn: elem.Item.isbn,
-      title: elem.Item.title,
-      author: elem.Item.author,
-      publisherName: elem.Item.publisherName,
-      salesDate: elem.Item.salesDate,
-      itemCaption: elem.Item.itemCaption,
-      itemPrice: elem.Item.itemPrice,
-      imageUrl: imageUrlHttps,
-      affiliateUrl: elem.Item.affiliateUrl,
-    };
-  });
 }
