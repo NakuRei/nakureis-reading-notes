b96c30ef7ae5d3ee05f5766e3fa00c9f3d5a1e56
NakuRei <118409499+NakuRei@users.noreply.github.com>
2023-11-19 01:34:28 +0900

    fix: adjust font size of header title (#40)
    Fixes #38

--- next-app/app/_components/layouts/Header.tsx
+++ next-app/app/_components/layouts/Header.tsx
@@ -31,9 +31,9 @@ export default function Header(props: HeaderProps) {
   return (
     <header className={`${props.className}`}>
       <div className="navbar">
-        <div className="navbar-start">
+        <div className="navbar-start w-2/3">
           <button
-            className="btn btn-ghost normal-case truncate inline-block max-w-full text-lg"
+            className="btn btn-ghost normal-case truncate inline-block max-w-full text-base sm:text-lg"
             onClick={moveToHome}
           >
             {props.appName}
