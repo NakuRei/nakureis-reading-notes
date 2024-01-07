f853405b40ce061872a99a6ed1747ae022ecd286
NakuRei <118409499+NakuRei@users.noreply.github.com>
2024-01-07 15:50:28 +0900

    fix: adjust article display for medium window sizes (#71)
    Implement a temporary fix for the issue where articles with code
    blocks are cut off in medium-sized window views. It's a
    stopgap measure while a more comprehensive, responsive design solution
    is developed.
    
    Fixes #67

--- next-app/app/_components/markdown/CustomPre.tsx
+++ next-app/app/_components/markdown/CustomPre.tsx
@@ -1,7 +1,11 @@
 import { Components } from 'react-markdown';
 
 const CustomPre: Components['pre'] = (props) => {
-  return <pre className="bg-transparent p-0 m-0">{props.children}</pre>;
+  return (
+    <pre className="max-md:max-w-none lg:max-w-md xl:max-w-none bg-transparent p-0 m-0">
+      {props.children}
+    </pre>
+  );
 };
 
 export default CustomPre;
